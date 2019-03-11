b63141b53f8d69e6f58ec30beea60eeb1c22f40f
Zhou Jialiang <zjl_sempre@163.com>
2019-03-11 20:36:22 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2018/7/16 21:51
-# @Author  : zhoujl
-# @Site    : 
-# @File    : run.py
-# @Software: PyCharm
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+################################################################################
+#
+# Copyright (c) 2019 ***.com, Inc. All Rights Reserved
+# The Proxy-Pool project
+################################################################################
+"""
+动态代理池 -- 主接口
+
+Usage: nohup python run.py >/dev/null 2>&1 & (无日志后台运行)
+Authors: Zhou Jialiang
+Email: zjl_sempre@163.com
+Date: 2018/7/15
+"""
 from api import app
 from scheduler import Scheduler
 from config import FLASK_HOST, FLASK_PORT
